5d5b6faaaaf2898b23f2d7e504a1ee8c5e7a26ee
aben20807 <aben20807@gmail.com>
2018-09-29 10:01:57 +0800

    Initialize members when malloc is successful
    When malloc fails, there is no any member being able
    to be accessed. Therefore, initialize only when malloc
    is successful.

--- queue.c
+++ queue.c
@@ -25,8 +25,11 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
