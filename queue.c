f7f66adab3721265f902171290980735e4666a00
aben20807 <aben20807@gmail.com>
2018-09-29 11:01:03 +0800

    Implement q_insert_head
    First, check if q is NULL, malloc for new head when
    answer is not othrewise return false. In addtion,
    if malloc for value is fail need to free node before
     returning false.

--- queue.c
+++ queue.c
@@ -50,14 +50,28 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL)
+        goto fail;
+
     list_ele_t *newh;
-    /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if ((newh = malloc(sizeof(list_ele_t))) == NULL)
+        goto fail;
+
+    int len = strlen(s) + 1;
+    if ((newh->value = malloc(len)) == NULL)
+        goto fail_and_free;
+
+    strncpy(newh->value, s, len);
     newh->next = q->head;
     q->head = newh;
+    q->size++;
     return true;
+
+fail_and_free:
+    free(newh);
+    newh = NULL;
+fail:
+    return false;
 }
 
 
